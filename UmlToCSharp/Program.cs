baf1b3f4ba67f9ade5a67f2330c47ab204094ba1
AlibekovTS <AlibekovTS@pda.su>
2022-10-21 16:45:17 +0300

    last add

--- UmlToCSharp/Program.cs
+++ UmlToCSharp/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more information
+// See https://learn.microsoft.com/ru-ru/dotnet/standard/base-types/regular-expressions
+// See https://gitlab.pba.su/visary/issues/-/wikis/domain-plantuml
+Console.WriteLine("Hello, World!");
